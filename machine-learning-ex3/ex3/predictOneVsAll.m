40ad4310013975ac2bb37aa07625901b3f29c39d
Ram81 <ramramrakhya81@gmail.com>
2016-12-24 17:01:05 +0530

    One-vs-all classfier Prediction

--- machine-learning-ex3/ex3/predictOneVsAll.m
+++ machine-learning-ex3/ex3/predictOneVsAll.m
@@ -30,6 +30,13 @@ X = [ones(m, 1) X];
 %       for each row.
 %       
 
+for i =1:m
+	RX=repmat(X(i,:),num_labels,1);
+	RX=RX.*all_theta;
+	SX=sum(RX,2); 
+	[val,index]=max(SX);
+	p(i)=index
+end
 
 
 
