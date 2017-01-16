bec26c5fa46bb9ebea1f51055fcd952952a7b87a
Ram81 <ramramrakhya81@gmail.com>
2017-01-16 16:03:59 +0530

    PAC Projection of Data Done

--- machine-learning-ex7/ex7/projectData.m
+++ machine-learning-ex7/ex7/projectData.m
@@ -18,7 +18,13 @@ Z = zeros(size(X, 1), K);
 %                    projection_k = x' * U(:, k);
 %
 
-
+for i = 1:size(X,1),
+	for j = 1:K,
+		x1 = X(i,:)';
+		projection_k = x1' * U(:,j);
+		Z(i,j)=projection_k;
+	end
+end
 
 
 % =============================================================
